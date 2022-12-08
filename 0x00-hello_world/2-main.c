0c8095c580a580e86aba902f42bcfae2b8c62fad
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-08 12:19:54 -0800

    added the 2-assembler shell file

---
+++ 0x00-hello_world/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * Entry point 
+ *
+ * Return 0 (Success)
+ *
+ */
+
+int main(void)
+{
+    printf("Hello World\n");
+    puts("I am now learnig C programming language\n");
+    return (0);
+}

5b482ade5fb9dbf8040f95d6482bffc52c550c5a
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-08 14:30:50 -0800

    updated the 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /*
- * main - This prints with proper grammer, but the outcome is a piece of art
+ * main - This prints with proper grammer, but the outcome is a piece of art,
  *
  * Return: This returns 0
  */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art\n");
+printf("with proper grammer, but the outcome is a piece of art,\n");
 return (0);
 }

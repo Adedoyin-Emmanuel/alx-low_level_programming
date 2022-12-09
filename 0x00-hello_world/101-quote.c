8b78dd85e3a033369975334c1f1601f8a423cc9c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-08 19:27:00 -0800

    added the last mandatory task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* main - This prints and that piece of art is userful" - Dora Korpar 2015-10-19"
+ * Return: This returns 0
+ *
+ */
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpa 2015-10-19\n");
+return (0);
+}

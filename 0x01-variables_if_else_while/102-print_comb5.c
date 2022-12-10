d7b90e3fb4090e3910d42a75721453fcec8f1131
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-09 21:52:00 -0800

    updated the last task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');

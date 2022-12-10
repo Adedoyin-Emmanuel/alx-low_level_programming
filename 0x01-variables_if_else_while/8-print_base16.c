91bab2cb29ded1b08c790c06dd73d01114aa2743
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-09 20:33:37 -0800

    updated the 8th task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,17 +7,14 @@
  */
 
 int main(void) {
-  // Print numbers 0-9
   for (int i = 0; i < 10; i++) {
     putchar('0' + i);
   }
 
-  // Print letters a-f
   for (int i = 0; i < 6; i++) {
     putchar('a' + i);
   }
 
-  // Print a new line
   putchar('\n');
 
   return 0;

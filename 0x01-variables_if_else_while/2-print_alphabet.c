eac7aa1107a26af00fda63dee0c39194301f999c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-09 19:50:17 -0800

    updated the 3rd task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,10 +10,12 @@ int main(void)
 {
 	char alpha;
 
-	for(alpha = "a"; alpha <= "z"; alpha++)
+	for(alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-		putchar("%c\n", alpha);
+		putchar(alpha);
+		putchar('\n');
 	}
+	
 
 	return (0);
 }

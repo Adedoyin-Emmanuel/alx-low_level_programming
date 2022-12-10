2601e113e600e62f41907967e0fab226cf8624cf
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-09 19:33:06 -0800

    added task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - receives no argument
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char aplha;
+
+	for(alpha = "a"; alpha <= "z"; alpha++)
+	{
+		printf("%c\n", alpha);
+	}
+
+	return (0);
+}

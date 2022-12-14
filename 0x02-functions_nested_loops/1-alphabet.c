25ca156a01724f1a8b6412c091deb90034ce4705
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-13 16:01:12 -0800

    updated the 2nd task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,6 +6,16 @@
  *Return: Always (0) Success
  */
 
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter < 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
 int main(void)
 {
 	print_alphabet();

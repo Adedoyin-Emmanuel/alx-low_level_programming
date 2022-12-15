6894893955119a2933f6e160687349951da7157e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-15 15:15:08 -0800

    ul
    dated the 2nd task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * mul - a function that multiplies two integers
+  * @c: the parameter of the function
+  *
+  * Return: 1 (Success)
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,4 +3,5 @@
 int _putchar(int);
 int _isupper(int);
 int _isdigit(int);
+int mul(int a, int b);
 #endif

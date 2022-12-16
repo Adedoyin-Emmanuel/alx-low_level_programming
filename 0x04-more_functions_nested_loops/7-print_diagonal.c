58e8f3e60155e5d30c5261a2fa90e71ddedb84d9
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-15 19:40:30 -0800

    updated the 7th task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,17 @@
 void print_diagonal(int n)
 {
 	int i, j;
-
-	for (int i =0; i < n; i++)
+	
+	if (n > 0)
 	{
-		for (j = 0; j < i; j++)
+		for (int i =0; i < n; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('*\n');
 		}
-		_putchar('*\n');
 	}
+	_putchar('\n');
 }

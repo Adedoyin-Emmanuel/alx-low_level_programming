1aab6932047cda11a4b75b333faee8e261532158
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-15 18:31:36 -0800

    updated the 5th task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,11 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if ( num > 9)
+			{
 				_putchar((j / 10 ) + '0');
+			}
 		}
-		_putchar(j + '0');
+		_putchar((j% 10) + '0');
 	}
 	_putchar('\n');
 }

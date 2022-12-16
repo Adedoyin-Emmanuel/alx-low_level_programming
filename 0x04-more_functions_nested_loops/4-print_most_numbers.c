a22bacbf34b8d3f172bfd74a14ce07cd0199d963
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-15 17:40:38 -0800

    updated the 4th tsk

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- *print_most_numbers - prints the numbers from 0 to 9 
- *followed by a new line
- *does not print 2 and 4
+ *print_most_numbers - Prints the numbers from 0 to 9 
+ *except from 2 and 4
  */
 
 void print_most_numbers(void)

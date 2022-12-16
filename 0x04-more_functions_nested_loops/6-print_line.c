1c3e784185dc5ee5e87b49c541b600129642e562
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-15 19:04:57 -0800

    updated the 6th task

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,8 @@ void print_line(int n)
 	int i;
 	
 	/* check if n is 0 or less*/
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}else{
-
-		for (i = 0; i <= n; i++)
-		{
+	if (n > 0)
+		for (i = 0; i < n; i++)
 			_putchar('_');
-		}
-	}
+	_putchar('\n');
 }

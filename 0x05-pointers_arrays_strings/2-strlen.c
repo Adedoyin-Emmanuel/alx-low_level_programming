9e1ba3bba157c890b0a071aef21120092c8dfca0
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-19 11:53:53 -0800

    added the 2nd file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_streln - returns the length of a string
+ *@s: is the parameter
+ *
+ *Return: the string length
+ */
+
+int _strlen(char *s)
+{
+	int length = strlen(*s);
+
+	return (length);
+}

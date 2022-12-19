f6d6618bb42c92baa7af73c4edb2aab654dea018
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-19 15:41:58 -0800

    updated the 5th task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *rev_string - a function that reverses a string
+ *@s: the parameter for the function
+ */
+
+void rev_string(char *s)
+{
+	int i, result;
+	
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	for (i = i -1; i >= 0; i--)
+	{
+		result += s[i];
+	}
+	*s = result;
+	return (*s);
+}

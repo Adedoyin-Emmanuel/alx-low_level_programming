d0430c11c11a98ef0ab230c7034e62fc91f9ae3d
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2022-12-19 12:04:02 -0800

    updated the 2nd task

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  *_streln - returns the length of a string
@@ -9,7 +10,7 @@
 
 int _strlen(char *s)
 {
-	int length = _strlen(*s);
+	int length = strlen(*s);
 
 	return (length);
 }

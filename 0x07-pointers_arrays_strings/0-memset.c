2906ebc68de4c1c41a81dffceb22efa62fc7454d
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-01-02 15:45:41 -0800

    Fixed the betty errors for 0-memset.c file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
  *_memset - a function that fills memory with a constant byte
  *@s: the pointer pointing to n
  *@n: the number bytes of the memory area
- *@b the constant byte
+ *@b: the constant byte
  *
  *Return: a pointer to the memory area
  */
@@ -12,6 +12,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	char *ptr_s = &*s;
+
 	memset(s, b, n);
-	return ptr_s;
+	return (ptr_s);
 }

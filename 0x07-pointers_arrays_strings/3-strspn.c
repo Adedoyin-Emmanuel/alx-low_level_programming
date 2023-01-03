baa65ae0b230807158fd55ea55b86e37a0e3128a
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-01-02 17:09:01 -0800

    Added the 3-strspn.c file

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strspn - get the length of a prefix substring
+ *@s: the string segment
+ *@accept: the bytes parameter from which the function returns 
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int byte_number = strspn*(s, accept);
+
+	return (byte_number);
+}

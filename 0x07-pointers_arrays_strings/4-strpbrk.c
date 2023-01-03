e51540f1d7b3bb5ba6f49b22e9e754029f116918
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-01-02 17:57:03 -0800

    updated the 4th task to suite betty

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,12 +5,12 @@
 *_strpbrk - a function that searches a string for any of a set of bytes
 *@s: the string the search is performed on
 *@accept: the parameter for the bytes check
-*Return: a pointer to the byte in s 
+*Return: a pointer to the byte in s
 */
 
 char *_strpbrk(char *s, char *accept)
 {
 	char *ptr_result = strpbrk(s, accept);
 
-	return ptr_result;
+	return (ptr_result);
 }

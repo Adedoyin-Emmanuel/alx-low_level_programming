0c1035e3d3d80f5eceb8629fd531a9cc03aa3bba
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-01-04 15:54:27 -0800

    Added the main task file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,2 +1,12 @@
 #include "main.h"
 
+/**
+ *_puts_recursion - prints a string, followed by a new line
+ *@s: the parameter to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar("%", s);
+	
+}
